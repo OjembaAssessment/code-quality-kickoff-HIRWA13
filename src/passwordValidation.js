7d5f3c43ef4b97afa17c26d516fe299948b61421
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-09 19:35:44 +0200

    feat: update the isValidPassword function

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -10,23 +10,22 @@ export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
 
-  // * * * YOUR CODE GOES IN HERE ... * * *
-const specialCharsRegex = /[!@#$%^&*()\-+={}[\]:;"'<>,.?\/|\\]/
-const checkCharAndNumRegex = /^[a-zA-Z0-9]$/
-const atLeastOneCharRegex = /^[a-zA-Z]/
-const atleastOneNumberRegex = /\d/
-const stringLengthRegex = /^.{10}$/
-
 if(password.length !== 10) return false;
-if(checkCharAndNumRegex.test(password)) return true;
-if(specialCharsRegex.test(password)) return false;
-if(atLeastOneCharRegex.test(password)) return false;
-if(atleastOneNumberRegex.test(password)) return false;
-if(forbiddenPasswords.includes(password)) return false;
+if (!/\d/.test(password)) return false;
+if (!/[a-zA-Z]/.test(password)) return false;
+if (!/^[a-zA-Z0-9]+$/.test(password)) return false;
+if (/^[A-Z0-9]+$/.test(password) || /^[a-z0-9]+$/.test(password)) return false;
+if (forbiddenPasswords.includes(password)) return false;
 
-
+const passwordSequence = ['0123456789', '9876543210'];
+  for (let sequence of passwordSequence) {
+    for (let index = 0; index <= sequence.length - 4; index++) {
+      const thirdSequence = sequence.substr(index, 4);
+      if (password.includes(thirdSequence)) return false;
+    }
+  }
 
 const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
-  return true
+  return true;
 }
