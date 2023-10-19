124dbe5c10c6efaccf405b6effa0c7d76453201d
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-19 10:54:59 +0200

    feat: add string concatination exercise

---
+++ src/exercises/stringConcatenation.js
@@ -0,0 +1,8 @@
+function stringConcat (string1, string2) {
+    return `${string1} ${string2}`;
+}
+
+// test manually
+
+console.log(stringConcat('hello', 'world'))
+console.Console.log(stringConcat('here', 'we are'))
