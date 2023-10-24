8932476417f9dbc07e30736572f00def9030fc8f
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-24 12:48:50 +0200

    feat: add multiplyNumbers function

---
+++ src/exercisees/calculate-product.js
@@ -0,0 +1,6 @@
+const multiplyNumbers = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
+}
+// tests:
+console.log(multiplyNumbers(2, 3))
+console.log(multiplyNumbers(4, 5))
