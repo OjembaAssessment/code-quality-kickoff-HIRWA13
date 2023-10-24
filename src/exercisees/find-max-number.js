37819e2a79252aae2cc7cf7508762acd169331ec
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-24 12:45:32 +0200

    feat: add findMaxNumber function

---
+++ src/exercisees/find-max-number.js
@@ -0,0 +1,8 @@
+const findMaxNumber = (firstNumber, secondNumber, thirdNumber) => {
+    return Math.max(firstNumber, secondNumber, thirdNumber);
+}
+// tests:
+console.log(findMaxNumber(0, 10, 5))
+console.log(findMaxNumber(0, -10, -2))
+console.log(findMaxNumber(1, 0, 2))
+console.log(findMaxNumber(30, 3, 10))
