0bfbd75b1d4abf54a3a8327d54dacce6f5f5f86b
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-25 10:33:11 +0200

    refactor: use arrow function

--- src/exercisees/remove-duplicates.js
+++ src/exercisees/remove-duplicates.js
@@ -1,6 +1,4 @@
-function removeDuplicates (arr){
-    const newArr = Array.from(new Set(arr));
-}
+const removeDuplicates = (arr) => Array.from(new Set(arr));
 
 // tests:
 console.log(removeDuplicates([1, 2, 3, 3, 4, 5, 5, 6]));
